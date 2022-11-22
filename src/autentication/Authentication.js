4b8a9b92ff9867247b9cc3a7f1dc4f97c343b917
Erica Rodrigues <ericarodrigues884@gmail.com>
2022-11-22 12:25:17 +0000

    login form

--- src/autentication/Authentication.js
+++ src/autentication/Authentication.js
@@ -19,11 +19,11 @@ const Background = styled.div`
 
 const Container = styled.div`
     background-color: ${props => props.register ? "#052F53" : "#007370"};
-    max-width: 1080px;
+    max-width: 995px;
     margin: 10rem;
-    height: 600px;
+    height: 500px;
     padding: 30px;
-    border-radius: 50px;
+    border-radius: 40px;
     display: flex;
     position: relative;
     flex-direction: row;
@@ -32,6 +32,7 @@ const Container = styled.div`
     justify-content: space-around;
     align-items: center;
     transition: all 1s ease-in-out;
+    
 
     & .modeChanger {
         display: flex;
@@ -39,6 +40,7 @@ const Container = styled.div`
         flex-direction: column;
         width: 35%;
         gap: 50px;
+
         
         & .MuiButtonBase-root {
             color: #000;
@@ -48,15 +50,17 @@ const Container = styled.div`
             font-family: 'Reem Kufi', sans-serif;
 
         }
+    
     }
 `
 
 const MovingContainer = styled.div`
     background-color: #E5E5E5;
     position: absolute;
-    width: 40%;
-    height: 120%;
-    left: 5%;
+    width: 41%;
+    height: 110%;
+    left: 3%;
+    border-radius: 30px;
     transition: all 1s ease-in-out;
 `
 
@@ -66,19 +70,19 @@ function Authentication() {
         <Background>
             <Container register={mode === 0}>
                 <div className='modeChanger'>
-                    <h2>Possui conta?</h2>
+                    <h2 style={{color:'white', fontSize:'30px'}}>Possui conta?</h2>
                     <Button onClick={() => {
                         setMode(1)
                     }}>Entrar</Button>
                 </div>
                 <div className='modeChanger'>
-                    <h2>Possui conta?</h2>
+                    <h2 style={{color:'white', fontSize:'30px'}}>Não possui conta?</h2>
                     <Button onClick={() => {
                         setMode(0)
-                    }}>Entrar</Button>
+                    }}>Registar-se</Button>
                 </div>
                 <MovingContainer style={mode === 0 ? {transform: "translateX(125%)"} : null}>
-                    {mode === 0 ? <LoginForm /> : <RegisterForm />}
+                    {mode === 0 ? <RegisterForm /> : <LoginForm />}
                 </MovingContainer>
             </Container>
         </Background >
--- src/autentication/LoginForm.js
+++ src/autentication/LoginForm.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+import ClearIcon from '@mui/icons-material/Clear';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
+
+
+const Container = styled.div`
+  display:'flex';
+  
+`
+
 
 function LoginForm() {
   return (
-    <div>LoginForm</div>
+    <Box
+    component="form"
+    sx={{
+      '& .MuiTextField-root': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <div> 
+    <TextField 
+          required
+          id="username"
+          label="Required"
+          defaultValue="Username"
+        />
+    </div>
+    </Box>
   )
 }
 
