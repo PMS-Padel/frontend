d44e0c35fe352f125c2b1f13bf61f2557a02e788
JoaoCupido <jcupido13@gmail.com>
2022-11-23 19:27:48 +0000

    correção da imagem e inserção de biblioteca na página de info de Padel

--- src/infoPadel.js
+++ src/infoPadel.js
@@ -1,8 +1,22 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { HeaderLanding } from './components/general/HeaderLandingPage';
+import { useState } from 'react';
+import styled from "styled-components";
+import NavBar from "./components/general/NavBar";
+import Row from "./components/general/Row";
+
+const AccountButton = styled(AccountCircleIcon)`
+  color: white;
+  position: absolute; 
+  top: 0%;
+  right: 10%
+`
+
 export function InfoPadel(){
     return(<>
-    <div>
+        <div>
 
-    </div>
+        </div>
     </>
     );
 }
