f65b5a66c2e0190a7df347cef1cbfc8f7fe698fe
JoaoCupido <jcupido13@gmail.com>
2022-11-28 12:35:43 +0000

    inicialização da página de info do Padel (sem responsividade)

--- src/infoPadel.js
+++ src/infoPadel.js
@@ -1,9 +1,14 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { HeaderLanding } from './components/general/HeaderLandingPage';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styled from "styled-components";
 import NavBar from "./components/general/NavBar";
 import Row from "./components/general/Row";
+import App from "./App";
+import backgroundPic from "./img/JogadorTorneiosInicialBackground.png";
+import rectangleImg from "./img/InfoPadelRoundedWhiteRectangle.png";
+import imagePexels_1 from "./img/PexelsPaddleImage1.png";
+import imagePexels_2 from "./img/PexelsPaddleImage2.png";
 
 const AccountButton = styled(AccountCircleIcon)`
   color: white;
@@ -14,9 +19,17 @@ const AccountButton = styled(AccountCircleIcon)`
 
 export function InfoPadel(){
     return(<>
-        <div>
+        <NavBar />
+        <div style={{ position: "relative", top: 0, left: 0 }}>
+            <img src={backgroundPic} alt="background" style={{ width: "100%", position: 'relative', zIndex: -10, objectFit: "cover", top: 0, left: 0 }} />
+            <h1 style={{ textAlign: 'center', fontSize: "40px", color: "white", position: 'absolute', bottom: "90%", left: "5%", zIndex: "-1"}}>Descobre mais sobre o teu desporto!</h1>
+            <img src={rectangleImg} alt="rectangleInfo" style={{ width: "94%", height: "83%", borderRadius: "1%", position: 'absolute', zIndex: -10, objectFit: "cover", top: 400, left: 45 }} />
+            <img src={imagePexels_1} alt="imageOne" style={{ width: "35%", position: 'absolute', zIndex: -10, top: 401, left: 1167 }} />
 
+            <img src={imagePexels_2} alt="imageTwo" style={{ width: "35%", position: 'absolute', zIndex: -10, top: 1501, left: 46 }} />
         </div>
     </>
     );
 }
+
+export default InfoPadel;
