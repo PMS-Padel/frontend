0913d347ccded23b049c15bc7c23fc3adf5adec2
JoaoCupido <jcupido13@gmail.com>
2022-11-28 12:41:11 +0000

    adição de texto no infoPadel (sem responsividade)

--- src/infoPadel.js
+++ src/infoPadel.js
@@ -23,10 +23,16 @@ export function InfoPadel(){
         <div style={{ position: "relative", top: 0, left: 0 }}>
             <img src={backgroundPic} alt="background" style={{ width: "100%", position: 'relative', zIndex: -10, objectFit: "cover", top: 0, left: 0 }} />
             <h1 style={{ textAlign: 'center', fontSize: "40px", color: "white", position: 'absolute', bottom: "90%", left: "5%", zIndex: "-1"}}>Descobre mais sobre o teu desporto!</h1>
-            <img src={rectangleImg} alt="rectangleInfo" style={{ width: "94%", height: "83%", borderRadius: "1%", position: 'absolute', zIndex: -10, objectFit: "cover", top: 400, left: 45 }} />
+            <img src={rectangleImg} alt="rectangleInfo" style={{ width: "94%", height: "75%", borderRadius: "1%", position: 'absolute', zIndex: -10, objectFit: "cover", top: 400, left: 45 }} />
+            <h2 style={{ textAlign: 'left', fontSize: "30px", color: "#15608A", position: 'absolute', bottom: "75%", left: "5%", zIndex: "-1"}}>Padel é um desporto de raquete praticado em pares.
+                <br/>O mesmo tem um sistema de jogo  semelhante ao ténis, onde os jogadores<br/>têm de
+                passar a bola por  cima da rede tendo obrigatoriamente de acertar<br/>no campo adversário.
+                Ao contrário do que a normalmente pensamos,<br/>a bola de padel é diferente da bola de ténis,
+                apesar da mesma aparência,<br/> as bolas de padel têm menor pressão o que resulta num ressalto
+                diferente.</h2>
             <img src={imagePexels_1} alt="imageOne" style={{ width: "35%", position: 'absolute', zIndex: -10, top: 401, left: 1167 }} />
-
             <img src={imagePexels_2} alt="imageTwo" style={{ width: "35%", position: 'absolute', zIndex: -10, top: 1501, left: 46 }} />
+            <h2 style={{ textAlign: 'center', fontSize: "30px", color: "#15608A", position: 'absolute', bottom: "50%", left: "45%", zIndex: "-1"}}>Descobre mais sobre o teu desporto!</h2>
         </div>
     </>
     );
