899f94d3b6e3fa039069f03dd4b7d4ca2e5a0925
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2022-12-04 18:27:00 +0000

    textbox "width %"

--- src/autentication/LoginForm.js
+++ src/autentication/LoginForm.js
@@ -27,7 +27,7 @@ function LoginForm() {
 
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        '& .MuiTextField-root': { m: 1, width: '74%' },
       }}
       noValidate
       autoComplete="off"
