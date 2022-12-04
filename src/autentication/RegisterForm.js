07dd9a941afe524664329e8f7d5c5835f982c09f
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2022-12-04 19:07:25 +0000

    mudei os campos

--- src/autentication/RegisterForm.js
+++ src/autentication/RegisterForm.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+import ClearIcon from '@mui/icons-material/Clear';
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Box from '@mui/material/Box';
+import { Button, Checkbox } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 function RegisterForm() {
   return (
-    <div>RegisterForm o</div>
+    <>
+    </>
+
   )
 }
 
