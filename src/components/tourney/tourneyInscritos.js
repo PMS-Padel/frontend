0554f48dee24f8049e28409c660ee4a552eb124f
JoaoCupido <jcupido13@gmail.com>
2023-01-01 21:16:25 +0000

    Exportar para Excel a tabela de equipas de um torneio e ajuste de cores de texto de inscritos por parte do tipo de utilizador/conta

--- src/components/tourney/tourneyInscritos.js
+++ src/components/tourney/tourneyInscritos.js
@@ -1,13 +1,15 @@
 import axiosConfig from "../../axiosConfig";
-import React, { useState, useEffect,} from "react";
+import React, {useState, useEffect, useRef,} from "react";
 import LoadingPopup from "../general/Loading";
 import AlertPopup from "../general/AlertPopup";
-import { Typography, Box } from "@mui/material";
+import {Typography, Box, Button} from "@mui/material";
+import {DownloadTableExcel} from "react-export-table-to-excel";
 
 
 export default function TourneyInscritos({tourney, user, storedAuth}) {
     const [dataTeams, setDataTeams] = useState({});
     const [update, setUpdate] = useState(false);
+    const tableRef = useRef(null);
 
     useEffect(() => {
         axiosConfig.get(`getteams/${tourney.id}`,{
@@ -36,29 +38,42 @@ export default function TourneyInscritos({tourney, user, storedAuth}) {
                         top: 700,
                         left: 150,
                         backgroundColor: "#FFFFFF",
-                        paddingLeft:'2rem',
-                        color: "#052F53"
+                        color: "#052F53",
+                        justifyContent: "center",
+                        textAlign: "center",
                     }}>
-                    <table style={{padding:'2rem', justifyContent:'center', textAlign: 'center'}}>
-                        <thead>
-                            <tr>
-                                <th>Nome da equipa</th>
-                                <th>Jogadores</th>
-                                <th>Níveis</th>
-                                <th>Data de inscrição</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {dataTeams.map((res)=>(
-                                <tr key={res.id}>
-                                <td style={{ padding: "0 15px" }}>{res.name}</td>
-                                <td style={{ padding: "0 15px" }}>{res.player1_id.name} | {res.player2_id.name} </td>
-                                <td style={{ padding: "0 15px" }}>{res.player1_id.level} | {res.player2_id.level} </td>
-                                <td style={{ padding: "0 15px" }}>{res.subscription_date}</td>
+                        <DownloadTableExcel
+                            filename={"ListaEquipasTorneio_" + tourney.name}
+                            sheet="ListaEquipas"
+                            currentTableRef={tableRef.current}
+                        >
+                            <Button variant="contained" style={{textTransform: 'none',
+                                backgroundColor: localStorage.getItem('loginForm') === 'admin' ? "#530508" : "#052F53",
+                                marginTop: "2rem"}}>
+                                Exportar para Excel
+                            </Button>
+                        </DownloadTableExcel>
+                        <table ref={tableRef} style={{margin:'2rem', justifyContent:'center', textAlign: 'center',
+                        color: localStorage.getItem('loginForm') === 'admin' ? "#530508" : "#052F53"}}>
+                            <thead>
+                                <tr>
+                                    <th>Nome da equipa</th>
+                                    <th>Jogadores</th>
+                                    <th>Níveis</th>
+                                    <th>Data de inscrição</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {dataTeams.map((res)=>(
+                                    <tr key={res.id}>
+                                    <td style={{ padding: "0 15px" }}>{res.name}</td>
+                                    <td style={{ padding: "0 15px" }}>{res.player1_id.name} | {res.player2_id.name} </td>
+                                    <td style={{ padding: "0 15px" }}>{res.player1_id.level} | {res.player2_id.level} </td>
+                                    <td style={{ padding: "0 15px" }}>{res.subscription_date}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </>
             )
@@ -75,7 +90,7 @@ export default function TourneyInscritos({tourney, user, storedAuth}) {
                     left: 150,
                     backgroundColor: "#FFFFFF",
                     paddingLeft:'2rem',
-                    color: "#052F53"
+                    color: localStorage.getItem('loginForm') === 'admin' ? "#530508" : "#052F53"
                 }}>
                     <h1>Carregando tabela...</h1>
                 </div>
