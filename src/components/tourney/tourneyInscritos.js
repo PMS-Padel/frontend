31752fea633f88e73f013eb95aa1e19dcf4a46df
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2023-01-01 11:44:57 +0000

    npm i @mui/x-data-grid

--- src/components/tourney/tourneyInscritos.js
+++ src/components/tourney/tourneyInscritos.js
@@ -2,13 +2,14 @@ import axiosConfig from "../../axiosConfig";
 import React, { useState, useEffect } from "react";
 import LoadingPopup from "../general/Loading";
 import AlertPopup from "../general/AlertPopup";
-
+import { DataGrid } from '@mui/x-data-grid';
 
 export default function TourneyInscritos() {
 
     return(
         <>
         <h1>inscrições</h1>
+        <DataGrid/>
 
         </>
     )
