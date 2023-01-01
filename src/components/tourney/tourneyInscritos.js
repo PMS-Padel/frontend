4cb455bb36a34572da8e89721f1adb8047fd65f0
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2023-01-01 11:23:35 +0000

    .

---
+++ src/components/tourney/tourneyInscritos.js
@@ -0,0 +1,15 @@
+import axiosConfig from "../../axiosConfig";
+import React, { useState, useEffect } from "react";
+import LoadingPopup from "../general/Loading";
+import AlertPopup from "../general/AlertPopup";
+
+
+export default function TourneyInscritos() {
+
+    return(
+        <>
+        <h1>inscrições</h1>
+
+        </>
+    )
+}
