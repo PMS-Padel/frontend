662b234dea61a0930a0c44f26f2c6fbc4c5f2ec4
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2023-01-01 19:36:54 +0000

    mapa de jogos

---
+++ src/components/tourney/tourneyMapaDeJogos.js
@@ -0,0 +1,15 @@
+
+import React, {useState, useEffect, useRef,} from "react";
+import {DownloadTableExcel} from "react-export-table-to-excel";
+import {Button} from "@mui/material";
+import axiosConfig from "../../axiosConfig";
+
+export default function TourneyMapaDeJogos({tourney, storedAuth}) {
+
+
+
+    return(
+        <>
+        </>
+    )
+}
