1f4df3dab945e768b34ef35b4c8bb2b98805f35d
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2023-01-01 19:33:54 +0000

    MapadeJogos

--- src/components/tourney/tourneyInscritos.js
+++ src/components/tourney/tourneyInscritos.js
@@ -1,30 +1,30 @@
 import axiosConfig from "../../axiosConfig";
-import React, {useState, useEffect, useRef,} from "react";
-import {DownloadTableExcel} from "react-export-table-to-excel";
-import {Button} from "@mui/material";
+import React, { useState, useEffect,} from "react";
+import LoadingPopup from "../general/Loading";
+import AlertPopup from "../general/AlertPopup";
+import { Typography, Box } from "@mui/material";
 
-export default function TourneyInscritos({tourney, storedAuth}) {
-    const [dataTeams, setDataTeams] = useState(undefined);
-    const tableRef = useRef(null);
+
+export default function TourneyInscritos({tourney, user, storedAuth}) {
+    const [dataTeams, setDataTeams] = useState({});
+    const [update, setUpdate] = useState(false);
 
     useEffect(() => {
-        if(tourney !== undefined)
-        {
-            axiosConfig.get(`getteams/${tourney.id}`, {
-                headers: {
-                    Authorization: 'Bearer ' + storedAuth
-                }
+        axiosConfig.get(`getteams/${tourney.id}`,{
+            headers: {
+                Authorization: 'Bearer ' + storedAuth
+            }
+        })
+            .then((res) => {
+                let {data} = res;
+                setDataTeams(data)
+                setUpdate(!update)
             })
-                .then((res) => {
-                    let {data} = res;
-                    setDataTeams(data)
-                })
-                .catch((error) => false);
-        }
-    },[tourney])
+            .catch((error) => false);
+        },[tourney])
 
     function mostraTabela(dataTeams){
-        if (tourney !== undefined && dataTeams !== undefined){
+        if (update){
             return(
                 <>
                     <div style={{
@@ -37,39 +37,28 @@ export default function TourneyInscritos({tourney, storedAuth}) {
                         left: 150,
                         backgroundColor: "#FFFFFF",
                         paddingLeft:'2rem',
-                        color: "#052F53",
-                        textAlign: "center",
-                        justifyContent: "center",
+                        color: "#052F53"
                     }}>
-                        <div>
-                            <DownloadTableExcel
-                                filename="users table"
-                                sheet="users"
-                                currentTableRef={tableRef.current}>
-                                <Button variant="contained" style={{textTransform: 'none', backgroundColor: localStorage.getItem('loginForm') === 'admin' ? "#530508" : "#052F53",
-                                    marginTop: "20px"}}>Exportar em Excel</Button>
-                            </DownloadTableExcel>
-                        </div>
-                        <table ref={tableRef} style={{padding:'2rem', justifyContent:'center', textAlign: 'center'}}>
-                            <thead>
-                                <tr>
-                                    <th>Nome da equipa</th>
-                                    <th>Jogadores</th>
-                                    <th>Níveis</th>
-                                    <th>Data de inscrição</th>
+                    <table style={{padding:'2rem', justifyContent:'center', textAlign: 'center'}}>
+                        <thead>
+                            <tr>
+                                <th>Nome da equipa</th>
+                                <th>Jogadores</th>
+                                <th>Níveis</th>
+                                <th>Data de inscrição</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dataTeams.map((res)=>(
+                                <tr key={res.id}>
+                                <td style={{ padding: "0 15px" }}>{res.name}</td>
+                                <td style={{ padding: "0 15px" }}>{res.player1_id.name} | {res.player2_id.name} </td>
+                                <td style={{ padding: "0 15px" }}>{res.player1_id.level} | {res.player2_id.level} </td>
+                                <td style={{ padding: "0 15px" }}>{res.subscription_date}</td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {dataTeams.map((res)=>(
-                                    <tr key={res.id}>
-                                    <td style={{ padding: "0 15px" }}>{res.name}</td>
-                                    <td style={{ padding: "0 15px" }}>{res.player1_id.name} | {res.player2_id.name} </td>
-                                    <td style={{ padding: "0 15px" }}>{res.player1_id.level} | {res.player2_id.level} </td>
-                                    <td style={{ padding: "0 15px" }}>{res.subscription_date}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                            ))}
+                        </tbody>
+                    </table>
                     </div>
                 </>
             )
