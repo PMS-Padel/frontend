2190f42a1c7484632d06fdf8f893f17f989f981e
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2023-01-04 05:16:49 +0000

    .

--- src/components/tourney/tourneyCalendario.js
+++ src/components/tourney/tourneyCalendario.js
@@ -5,13 +5,34 @@ import TextField from "@mui/material/TextField";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import NativeSelect from "@mui/material/NativeSelect";
+
 export default function TourneyCalendario({tourney, storedAuth}) {
+    const [dataTeams, setDataTeams] = useState({});
+    const [dataTeamsLength, setDataTeamsLength] = useState(0);
+    const [update, setUpdate] = useState(false);
 
+    useEffect(() => {
+        axiosConfig.get(`getteams/${tourney.id}`,{
+            headers: {
+                Authorization: 'Bearer ' + storedAuth
+            }
+        })
+            .then((res) => {
+                let {data} = res;
+                setDataTeams(data)
+                setUpdate(true)
+                setDataTeamsLength(dataTeams.length)
+            })
+            .catch((error) => false);
+        },[tourney])
 
+    function mostraTournamentCalendario(dataTeams,dataTeamsLength){
 
-    return(
-        <>
-               <div style={{
+        if(update){
+            return(
+                <>
+
+                    <div style={{
                         width: "80%",
                         height:"50rem",
                         borderRadius: "1%",
@@ -24,6 +45,8 @@ export default function TourneyCalendario({tourney, storedAuth}) {
                         paddingLeft:'2rem',
                         color: "#052F53"}}>
                         
+                        {dataTeams.slice(0, Math.ceil(dataTeams.length / 2)).map(()=>(
+                        <div>
                         <FormControl sx={{width:'10rem'}}>
                         <InputLabel variant="standard" htmlFor="uncontrolled-native">
                             Team
@@ -41,8 +64,18 @@ export default function TourneyCalendario({tourney, storedAuth}) {
                         </NativeSelect>
                         </FormControl>
 
-                        <TextField id="time" type="date" format="yyyy-mm-dd" inputProps={undefined} />;
+                        <TextField id="time" type="date" format="yyyy-mm-dd" inputProps={undefined} />
+                        </div>
+                    ))}
                     </div>
+                </>
+            )
+        }
+    }
+
+    return(
+        <>
+            {mostraTournamentCalendario(dataTeams,dataTeamsLength)}
         </>
     )
 }
